5b82ba0eb12ac3fa2fad0fb69088d3ba4bec165e
iCanTutoring <micahoforiappiah@gmail.com>
2025-07-08 00:13:24 +0100

    Create /packages page

---
+++ app/packages/page.jsx
@@ -0,0 +1,8 @@
+export default function PackagesPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Our Packages & Courses</h1>
+      <p>This page will list our KS2–KS4 offerings and pricing.</p>
+    </main>
+  );
+}
