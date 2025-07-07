732b4bc0120ddd56d0f4e805ba93a2611befdd5c
iCanTutoring <micahoforiappiah@gmail.com>
2025-07-07 23:17:45 +0100

    Add homepage route

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to iCan Tutoring</h1>
+      <p>This is your homepage.</p>
+      <a href="/book-trial">Book a Trial</a> | <a href="/sign-in">Sign In</a>
+    </main>
+  );
+}
