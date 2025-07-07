93b6fc03da826b57dc3b3ca619b4db80b057c174
iCanTutoring <micahoforiappiah@gmail.com>
2025-07-07 23:19:03 +0100

    Add book trial page

---
+++ app/book-trial/page.jsx
@@ -0,0 +1,8 @@
+export default function BookTrial() {
+  return (
+    <main>
+      <h1>Book a Free Trial</h1>
+      <p>This is the /book-trial page where visitors can request a free session.</p>
+    </main>
+  );
+}
